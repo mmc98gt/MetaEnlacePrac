eb110dbf4c9454ad6c7d4f4c214c14bb91ce3fc2
mmc98gt <mmcrealmadrid@hotmail.com>
2022-02-22 16:04:57 +0100

    cambios pequeños

--- Practicas/src/main/java/com/example/service/UsuarioService.java
+++ Practicas/src/main/java/com/example/service/UsuarioService.java
@@ -2,7 +2,8 @@ package com.example.service;
 
 import java.util.List;
 import java.util.Optional;
-
+import com.example.entity.Usuario;
+import com.example.entityDTO.UsuarioDTO;
 
 public interface UsuarioService {
 	public List<UsuarioDTO> findAllUsuarios();
